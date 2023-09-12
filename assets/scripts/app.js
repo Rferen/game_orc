311e85494bd874b614b6c9bfbdeb18f7cda87854
Tamás <dotomiro2@gmail.com>
2023-09-12 18:35:12 +0200

    add optional video

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,30 +7,38 @@ let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let isPlaying = false;
+let actionCounter = 0;
 
-adjustHealthBars(chosenMaxLife);
+async function uSure() {
+  if (
+    confirm('Azt állította MiniszterElnök Úr hogy áremelések lesznek ezigaz?')
+  ) {
+    // Igen
+    alert('Fake news!');
+    await playVideo('./assets/videos/Fake_news.mp4');
+  } else {
+    //  Nem
+    alert('Mondtál valamit vicceset és mindeki elfelejetette a kérdést!');
+    await playVideo('./assets/videos/Vicces_orban.mp4');
+  }
+}
 
-// function uSure (){
-//   if (confirm ("Azt állította MiniszterElnök Úr hogy áremelések lesznek ezigaz?")) {
-//     // Igen
-//     alert('Fake news!');
-//     playVideo("./assets/videos/Fake_news.mp4")
-//   } else {
-//     //  Nem
-//     alert('Mondtál valamit vicceset és mindeki elfelejetette a kérdést!');
-//     playVideo("./assets/videos/Vicces_orban.mp4")
-//   }
-// }
+function playOptional() {
+  actionCounter++;
 
-// function randomExecute(func) {
-//   const randomValue = Math.random();
-//   if (randomValue <= 0.10) {
-//     func();
-//   }
-// }
-// randomExecute(uSure);
+  if (actionCounter === 3) {
+    uSure();
+  }
+}
 
-async function playVideo(newSource){
+function randomExecute(func) {
+  const randomValue = Math.random();
+  if (randomValue <= 0.1) {
+    func();
+  }
+}
+
+async function playVideo(newSource) {
   videoSource.setAttribute('src', newSource);
   await video.load();
   video.currentTime = 0; // Videó visszaállítása az elejére
@@ -41,22 +49,26 @@ async function playVideo(newSource){
 function endRound() {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
-  if  (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('2/3 NYERTÉL MINT MINDIG!');
-    playVideo("./assets/videos/Fidesz_nyert.mp4")
+    playVideo('./assets/videos/Fidesz_nyert.mp4');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('Az ellenzék nyert!');
-    playVideo("./assets/videos/Mocskos_fidesz.mp4")
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
+    playVideo('./assets/videos/Mocskos_fidesz.mp4');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('Draw');
   }
 }
 
-async function suicide(){
-  await playVideo("./assets/videos/Biztos.mp4")
-  if (confirm ("Biztos meg vonod a 13 havi nyugdíjat?")) {
+async function suicide() {
+  await playOptional();
+
+  await playVideo('./assets/videos/Biztos.mp4');
+  if (confirm('Biztos meg vonod a 13 havi nyugdíjat?')) {
     // Igen
-    alert('A szavazóid elálltak mellőled mert meg vontad a 13 havi nyugdíjat ezért automatikusan vesztettél!');
+    alert(
+      'A szavazóid elálltak mellőled mert meg vontad a 13 havi nyugdíjat ezért automatikusan vesztettél!'
+    );
     const playerDamage = dealPlayerDamage(1000);
     currentPlayerHealth -= playerDamage;
   } else {
@@ -66,19 +78,23 @@ async function suicide(){
   endRound();
 }
 
-function easyWin(){
-  alert('Magyarország 99% beszopta a dumádat ezért tovább uralkodsz mint királyuk! Automatikusan nyertél!');
+async function easyWin() {
+  await playOptional();
+
+  alert(
+    'Magyarország 99% beszopta a dumádat ezért tovább uralkodsz mint királyuk! Automatikusan nyertél!'
+  );
   const monsterDamage = dealMonsterDamage(100000);
   currentMonsterHealth -= monsterDamage;
-  playVideo("./assets/videos/Brusszel_hibas.mp4");
+  playVideo('./assets/videos/Brusszel_hibas.mp4');
   endRound();
 }
 
-function attackMonster (mode) {
+function attackMonster(mode) {
   let maxDamage;
-  if(mode ==='NYUGDIJASOK'){
+  if (mode === 'NYUGDIJASOK') {
     maxDamage = NYUGDIJASOK;
-  } else if (mode === 'SZANKCIOK'){
+  } else if (mode === 'SZANKCIOK') {
     maxDamage = SZANKCIOK;
   }
   const damage = dealMonsterDamage(maxDamage);
@@ -86,31 +102,37 @@ function attackMonster (mode) {
   endRound();
 }
 
-function selfHarm() {
-  playVideo("./assets/videos/verd_ki.mp4");
+async function selfHarm() {
+  await playOptional();
+
+  playVideo('./assets/videos/verd_ki.mp4');
   const playerDamage = dealPlayerDamage(31);
   currentPlayerHealth -= playerDamage;
 }
 
-function attackHandler() {
+async function attackHandler() {
   attackMonster('NYUGDIJASOK');
-  playVideo("./assets/videos/Nyugdijasok_duhe_mortal.mp4");
+  await playVideo('./assets/videos/Nyugdijasok_duhe_mortal.mp4');
+  await playOptional();
 }
 
-function strongAttackHandler() {
+async function strongAttackHandler() {
   attackMonster('SZANKCIOK');
-  playVideo("./assets/videos/Szankciok.mp4");
+  await playVideo('./assets/videos/Szankciok.mp4');
+  await playOptional();
 }
 
-function healPlayerHandler(){
+async function healPlayerHandler() {
+  await playOptional();
+
   let healValue;
-  if(currentPlayerHealth >= chosenMaxLife - CIGI){
+  if (currentPlayerHealth >= chosenMaxLife - CIGI) {
     alert('Ennél táposabb már nem leszel!');
     healValue = chosenMaxLife - currentPlayerHealth;
   } else {
     healValue = CIGI;
   }
-  playVideo("./assets/videos/Kolbasz_eves.mp4");
+  playVideo('./assets/videos/Kolbasz_eves.mp4');
   increasePlayerHealth(healValue);
   currentPlayerHealth += healValue;
   endRound();
@@ -121,4 +143,7 @@ strongAttackBtn.addEventListener('click', strongAttackHandler);
 healBtn.addEventListener('click', healPlayerHandler);
 suicideBtn.addEventListener('click', suicide);
 winBTN.addEventListener('click', easyWin);
-selfHarmBtn.addEventListener('click', selfHarm)
+selfHarmBtn.addEventListener('click', selfHarm);
+
+adjustHealthBars(chosenMaxLife);
+randomExecute(uSure);
